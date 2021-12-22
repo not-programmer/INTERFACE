ec488d8c699fc920a2c51abdf7f4982dfb84a3a4
not-programmer <96480451+not-programmer@users.noreply.github.com>
2021-12-22 18:53:01 +0800

    Delete Transforming.kt

--- Transforming.kt
+++
@@ -1,7 +0,0 @@
-interface Transforming {
-    fun move(dx: Int, dy: Int)
-    fun resize(zoom: Int)
-    fun rotate(direction: RotateDirection, centerX: Int, centerY: Int)
-}
-
-enum class RotateDirection { CLOCKWISE, COUNTERCLOCKWISE }
