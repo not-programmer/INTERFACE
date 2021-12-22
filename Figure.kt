13d9d6b3ec9b8d25ccd93d8664485aab78cd1252
not-programmer <96480451+not-programmer@users.noreply.github.com>
2021-12-22 18:52:43 +0800

    Delete Figure.kt

--- Figure.kt
+++
@@ -1,5 +0,0 @@
-abstract class Figure(val id: Int) {
-    abstract fun move (dx: Int, dy: Int)
-    abstract fun resize (zoom: Int)
-    abstract fun rotate (direction: RotateDirection, centerX: Int, centerY: Int)
-}
